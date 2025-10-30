45d2548a05a39d3f18375b469c1af6a283f5ffce
Jaewon Park (박재원) <jedeop@jedeop.dev>
2025-10-30 23:47:54 +0900

    fix: dark mode support

--- src/components/LinkBlock.tsx
+++ src/components/LinkBlock.tsx
@@ -16,7 +16,7 @@ interface LinkBlockProps {
 export default function LinkBlock({ content }: LinkBlockProps) {
   const body = (
     <div
-      className={`flex items-center gap-2 py-1.5 px-5 rounded-full border-3 border-gray-200 ${content.life ? "border-dashed" : "bg-gray-200 "}`}
+      className={`flex items-center gap-2 py-1.5 px-5 rounded-full border-3 border-gray-200 dark:border-gray-700 ${content.life ? "border-dashed" : "bg-gray-200 dark:bg-gray-700"}`}
     >
       <content.Icon size={24} className="shrink-0" />
       <div className="font-bold">{content.title}</div>
